e9ee7396bb362c65fe466b06cbd2b5652d5df7ec
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-19 22:14:55 +0200

    comments removed

--- config/dbConn.js
+++ config/dbConn.js
@@ -5,7 +5,6 @@ const connectDB = async () => {
     await mongoose.connect(process.env.DATABASE_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      //   useCreateIndex: true,
     });
     console.log("MongoDB connected...");
   } catch (err) {
--- routes/root.js
+++ routes/root.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const path = require("path");
 
 router.get("^/$|index(.html)?", (req, res) => {
-  // res.sendFile("./views/index.html", { root: __dirnajme });
   res.sendFile(path.join(__dirname, "..", "views", "index.html"));
 });
 
